333119feff86ab0f5fbf90b3a854f08cd30d1954
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-23 23:31:24 +0100

    Create template to display list of tweets dynamically

--- OneDrive/Masaüstü/reps/django_tweet/django_tweet/tweetapp/views.py
+++ OneDrive/Masaüstü/reps/django_tweet/django_tweet/tweetapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def list_tweet(request):
-    return render(request,"tweetapp/list_tweet.html")
+    all_tweets= models.Tweet.objects.all()
+    tweet_dict = {"Tweets": all_tweets}
+    return render(request,"tweetapp/list_tweet.html",context=tweet_dict)
 
 def add_tweet(request):
     return render(request,"tweetapp/add_tweet.html")
