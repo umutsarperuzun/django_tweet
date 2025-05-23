fd528cf175559b4cd18b931faf4bfd8cad146b39
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-23 23:21:21 +0100

    Define Tweet model with nickname, message, and timestamp fields

--- OneDrive/Masaüstü/reps/django_tweet/django_tweet/tweetapp/models.py
+++ OneDrive/Masaüstü/reps/django_tweet/django_tweet/tweetapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Tweet(models.Model):
+    nickname= models.CharField(max_length=50)
+    message=models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Nickname: {self.nickname}, Message: {self.message} , Create_Time : {self.created_at}"
+
+    
