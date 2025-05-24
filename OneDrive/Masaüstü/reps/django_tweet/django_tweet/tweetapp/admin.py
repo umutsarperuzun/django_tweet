6f21bfb9d87d9cbf031b0382c7ef8d9a5d1f2e8f
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-24 20:04:10 +0100

    Customize Tweet model admin interface using fieldsets

--- OneDrive/Masaüstü/reps/django_tweet/django_tweet/tweetapp/admin.py
+++ OneDrive/Masaüstü/reps/django_tweet/django_tweet/tweetapp/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from tweetapp.models import Tweet #Import to model
 
+class TweetAdmin(admin.ModelAdmin): #For admin previllages management and specifications
+    fieldsets = [
+        ('Message Group',{"fields":["message"]}),
+        ('Nickname Group',{"fields":["nickname"]})
+    ]
+    # fields=['message','nickname']
 
-# Register your models here.
-admin.site.register(Tweet)
+
+
+admin.site.register(Tweet,TweetAdmin)
