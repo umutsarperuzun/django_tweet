972f5fd73e9fac4146222f013dbb1155fd9fcaf0
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-28 00:15:57 +0100

    Implement user login authentication functionality

--- OneDrive/Masaüstü/reps/django_tweet/django_tweet/django_tweet/settings.py
+++ OneDrive/Masaüstü/reps/django_tweet/django_tweet/django_tweet/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGIN_URL=""
+LOGIN_REDIRECT_URL="/"
